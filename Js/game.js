1dc4d34b39e2649c3d3b4a505073d6999408acd3
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-11 13:04:43 +0200

    LimpiandoC

--- Js/game.js
+++ Js/game.js
@@ -18,27 +18,27 @@ class Game {
     this.lives = 3;
     this.gameIsOver = false;
     this.gameIntervalId;
-    this.gameLoopFrequency = Math.round(1000/100); // 60fps
+    this.gameLoopFrequency = Math.round(1000/100); // 60fps / 100fps
   }
 
   start() {
-    // Set the height and width of the game screen
+   
     this.gameScreen.style.height = `${this.height}px`;
     this.gameScreen.style.width = `${this.width}px`;
 
-    // Hide the start screen
+    
     this.startScreen.style.display = "none";
-    // Show the game screen
+    
     this.gameScreen.style.display = "block";
 
-    // Executes the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
+    
     this.gameIntervalId = setInterval(() => {
       this.gameLoop()
     }, this.gameLoopFrequency)
   }
 
   gameLoop() {
-    console.log("in the game loop");
+    //console.log("in the game loop");
 
     this.update();
     
@@ -50,47 +50,45 @@ class Game {
   update() {
     this.player.move();
 
-    // Check for collision and if an obstacle is still on the screen
+    
     for (let i = 0; i < this.obstacles.length; i++) {
       const obstacle = this.obstacles[i];
       obstacle.move();
 
-      // If the player's car collides with an obstacle
+     
       if (this.player.didCollide(obstacle)) {
-        // Remove the obstacle element from the DOM
+       
         obstacle.element.remove();
-        // Remove obstacle object from the array
+       
         this.obstacles.splice(i, 1);
-        // Reduce player's lives by 1
+        
         this.lives--;
-        // Update the counter variable to account for the removed obstacle
+        
         i--;
-      } // If the obstacle is off the screen (at the bottom)
+      } 
       else if (obstacle.top > this.height) {
-        // Increase the score by 1
+        
         this.score++;
-        // Remove the obstacle from the DOM
+        
         obstacle.element.remove();
-        // Remove obstacle object from the array
+        
         this.obstacles.splice(i, 1);
-        // Update the counter variable to account for the removed obstacle
+        
         i--;
       }
     }
 
-    // If the lives are 0, end the game
+    
     if (this.lives === 0) {
       this.endGame();
     }
-
-    // Create a new obstacle based on a random probability
-    // when there is no other obstacles on the screen
+    
     if (Math.random() > 0.99 && this.obstacles.length < 3) {
       this.obstacles.push(new Obstacle(this.gameScreen));
     }
   }
 
-  // Create a new method responsible for ending the game
+  
   endGame() {
     this.player.element.remove();
     this.obstacles.forEach(function (obstacle) {
@@ -98,9 +96,9 @@ class Game {
     });
 
     this.gameIsOver = true;
-    // Hide game screen
+   
     this.gameScreen.style.display = "none";
-    // Show end game screen
+    
     this.gameEndScreen.style.display = "flex";
   }
 }
--- Js/script.js
+++ Js/script.js
@@ -9,7 +9,7 @@ window.onload = function () {
   });
 
   restartButton.addEventListener("click", function () {
-    // Call the restartGame function when the button is clicked
+    
     restartGame();
   });
 
@@ -20,12 +20,12 @@ window.onload = function () {
     game.start();
   }
 
-  // The function that reloads the page to start a new game
+ 
   function restartGame() {
     location.reload();
   }
 
-  // Function that handles keydown event
+  
   function handleKeydown(event) {
     const key = event.key;
     const possibleKeystrokes = [
@@ -35,11 +35,11 @@ window.onload = function () {
       "s",
     ];
 
-    // Check if the pressed key is in the possibleKeystrokes array
+    
     if (possibleKeystrokes.includes(key)) {
       event.preventDefault();
 
-      // Update player's directionX and directionY based on the key pressed
+      
       switch (key) {
         case "a":
           game.player.directionX = -2;
@@ -57,6 +57,6 @@ window.onload = function () {
     }
   }
 
-  // Add the handleKeydown function as an event listener for the keydown event
+  
   window.addEventListener("keydown", handleKeydown);
 };
